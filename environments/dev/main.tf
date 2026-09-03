6cafeab7261485be9c5bb4535198a8d62e76822e
Carel Assonkeng Takoumene <takoumenecarel@icloud.com>
2026-09-03 17:55:06 -0400

    Add Checkov configuration

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,3 +2,35 @@ resource "aws_s3_bucket" "test" {
   bucket = "ctl-dev-bootstrap-test"
 }
 
+resource "aws_s3_bucket_public_access_block" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_kms_key" "s3" {
+  description         = "KMS key for S3 encryption"
+  enable_key_rotation = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.s3.arn
+    }
+  }
+}
