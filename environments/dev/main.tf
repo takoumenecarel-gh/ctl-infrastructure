df9931628f831dc03a21f6a19d4b73dc6feeeb4c
Carel Assonkeng Takoumene <takoumenecarel@icloud.com>
2026-09-03 17:59:56 -0400

    Add Checkov configuration

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -34,3 +34,40 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "test" {
     }
   }
 }
+
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "s3_kms" {
+  statement {
+    sid    = "EnableRootPermissions"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+      ]
+    }
+
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_kms_key" "s3" {
+  description         = "KMS key for S3 encryption"
+  enable_key_rotation = true
+
+  policy = data.aws_iam_policy_document.s3_kms.json
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.s3.arn
+    }
+  }
+}
