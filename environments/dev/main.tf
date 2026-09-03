8ac2812a2dcf8e33ce382408b348c7e18f0a825b
Carel Assonkeng Takoumene <takoumenecarel@icloud.com>
2026-09-03 18:03:18 -0400

    Harden dev S3 bucket configuration

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -2,6 +2,10 @@ resource "aws_s3_bucket" "test" {
   bucket = "ctl-dev-bootstrap-test"
 }
 
+# --------------------------------------------------
+# S3 Public Access Block
+# --------------------------------------------------
+
 resource "aws_s3_bucket_public_access_block" "test" {
   bucket = aws_s3_bucket.test.id
 
@@ -11,6 +15,10 @@ resource "aws_s3_bucket_public_access_block" "test" {
   restrict_public_buckets = true
 }
 
+# --------------------------------------------------
+# S3 Versioning
+# --------------------------------------------------
+
 resource "aws_s3_bucket_versioning" "test" {
   bucket = aws_s3_bucket.test.id
 
@@ -19,21 +27,9 @@ resource "aws_s3_bucket_versioning" "test" {
   }
 }
 
-resource "aws_kms_key" "s3" {
-  description         = "KMS key for S3 encryption"
-  enable_key_rotation = true
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "test" {
-  bucket = aws_s3_bucket.test.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "aws:kms"
-      kms_master_key_id = aws_kms_key.s3.arn
-    }
-  }
-}
+# --------------------------------------------------
+# KMS
+# --------------------------------------------------
 
 data "aws_caller_identity" "current" {}
 
@@ -43,7 +39,8 @@ data "aws_iam_policy_document" "s3_kms" {
     effect = "Allow"
 
     principals {
-      type        = "AWS"
+      type = "AWS"
+
       identifiers = [
         "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
       ]
@@ -61,6 +58,10 @@ resource "aws_kms_key" "s3" {
   policy = data.aws_iam_policy_document.s3_kms.json
 }
 
+# --------------------------------------------------
+# S3 Encryption
+# --------------------------------------------------
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "test" {
   bucket = aws_s3_bucket.test.id
 
@@ -68,6 +69,23 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "test" {
     apply_server_side_encryption_by_default {
       sse_algorithm     = "aws:kms"
       kms_master_key_id = aws_kms_key.s3.arn
+    }
+  }
+}
+
+# --------------------------------------------------
+# S3 Lifecycle
+# --------------------------------------------------
+
+resource "aws_s3_bucket_lifecycle_configuration" "test" {
+  bucket = aws_s3_bucket.test.id
+
+  rule {
+    id     = "cleanup"
+    status = "Enabled"
+
+    expiration {
+      days = 30
     }
   }
 }
